8146b3f98f0ad60581bd646507b9382eb4f73f50
EliseuDev-Math <eliseualexandre496@gmail.com>
2026-06-13 17:24:34 -0400

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,15 +3,13 @@ import { fileURLToPath } from "url";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
-import { viteSingleFile } from "vite-plugin-singlefile";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-// https://vite.dev
 export default defineConfig({
-  base: "/3CNewsSite/", 
-  plugins: [react(), tailwindcss(), viteSingleFile()],
+  base: "/3CNewsSite/",
+  plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "src"),
